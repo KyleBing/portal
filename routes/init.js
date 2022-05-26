da230143019fd8d5d1be7c7bb6fca14816306c20
邴新科 <kylebing@163.com>
2022-05-26 17:10:22 +0800

    init； thumbs_up

--- routes/init.js
+++ routes/init.js
@@ -206,6 +206,22 @@ CREATE TABLE \`wubi_dict\`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 0 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for thumbs_up
+-- ----------------------------
+DROP TABLE IF EXISTS \`thumbs_up\`;
+CREATE TABLE \`thumbs_up\`  (
+  \`up_key\` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  \`up_count\` int(11) NOT NULL DEFAULT 0 COMMENT '点赞数',
+  \`link_address\` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '部署地址',
+  \`date_init\` datetime NOT NULL COMMENT '添加地址',
+  PRIMARY KEY (\`up_key\`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+
+
 SET FOREIGN_KEY_CHECKS = 1;
 `
         connection.query(sqlCreateTables, [], function (err, result) {
