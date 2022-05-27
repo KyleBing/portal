57bbaa82b3df1d4a5a1b5081694e2121cb30531e
邴新科 <kylebing@163.com>
2022-05-27 11:59:17 +0800

    发送的 ws 数据，使用统计数据结构

--- bin/ws.js
+++ bin/ws.js
@@ -8,7 +8,17 @@ const utility = require("../config/utility")
 // 创建一个 websocket 服务
 const wss = new WebSocketServer.Server({ port: 9999 })
 
-let timeIntervalHandle = null
+class WSMessage{
+    constructor(type, content) {
+        this.type = type
+        this.content = content
+    }
+    static type = {
+        thumbsUp: 'thumbs-up',
+        heartBeat: 'heart-beat',
+    }
+}
+
 
 // 创建连接
 wss.on("connection", ws => {
@@ -16,24 +26,28 @@ wss.on("connection", ws => {
     // 接收到 client 数据时
     ws.on("message", data => {
         // console.log(`客户端返回信息: ${data}`)
-        let receiveMessage = JSON.parse(data.toString())
-        if (receiveMessage.key){
-            utility.getDataFromDB([`UPDATE thumbs_up SET up_count=up_count + 1 WHERE up_key = '${receiveMessage.key}'`])
-                .then(resultUpdate => {
-                    utility.getDataFromDB([`select up_count from thumbs_up where up_key = '${receiveMessage.key}'`], true)
-                        .then(result => {
-                            if (result){
-                                let sendMessage = JSON.stringify({
-                                    key: receiveMessage.key,
-                                    count: result.up_count
-                                })
-                                wss.clients.forEach(client =>  {
-                                    console.log('111')
-                                    client.send(sendMessage)
-                                })
-                            }
-                        })
-                })
+        let receiveMessage = JSON.parse(data)
+        switch (receiveMessage.type){
+            case WSMessage.type.heartBeat:
+                ws.send(JSON.stringify(new WSMessage(WSMessage.type.heartBeat, 'pong')))
+                break;
+            case WSMessage.type.thumbsUp:
+                utility.getDataFromDB([`UPDATE thumbs_up SET up_count=up_count + 1 WHERE up_key = '${receiveMessage.content.key}'`])
+                    .then(resultUpdate => {
+                        utility.getDataFromDB([`select up_count from thumbs_up where up_key = '${receiveMessage.content.key}'`], true)
+                            .then(result => {
+                                if (result){
+                                    let sendMessage = new WSMessage(WSMessage.type.thumbsUp, {
+                                        key: receiveMessage.content.key,
+                                        count: result.up_count
+                                    })
+                                    wss.clients.forEach(client =>  {
+                                        client.send(JSON.stringify(sendMessage))
+                                    })
+                                }
+                            })
+                    })
+                break;
         }
     })
     ws.on("close", () => {
@@ -43,6 +57,7 @@ wss.on("connection", ws => {
         console.log("websocket server: 出错了")
     }
 })
+
 
 
 console.log("websocket 服务已运行在端口 9999")
