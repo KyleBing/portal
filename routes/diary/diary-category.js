38f7e18e3992cc09cf398001ef539c348fcb10c0
邴新科 <kylebing@163.com>
2022-12-08 11:26:01 +0800

    diary-category 请求规范化

--- routes/diary/diary-category.js
+++ routes/diary/diary-category.js
@@ -5,17 +5,20 @@ const utility = require('../../config/utility')
 const ResponseSuccess = require('../../response/ResponseSuccess')
 const ResponseError = require('../../response/ResponseError')
 
+const TABLE_NAME = 'diary_category' // 表名
+const DATA_NAME = '日记类别'          // 操作的数据名
+
 router.get('/list', (req, res, next) => {
     // query.name_en
     let sqlArray = []
-    sqlArray.push(` select * from diary_category order by sort_id asc`)
+    sqlArray.push(` select * from ${TABLE_NAME} order by sort_id asc`)
     utility
         .getDataFromDB( 'diary', sqlArray)
         .then(data => {
             if (data) { // 没有记录时会返回  undefined
                 res.send(new ResponseSuccess(data))
             } else {
-                res.send(new ResponseError('', '日记类别查询错误'))
+                res.send(new ResponseError('', `${DATA_NAME}查询错误`))
             }
         })
         .catch(err => {
@@ -27,7 +30,7 @@ router.post('/add', (req, res, next) => {
         .then(dataCategoryExistanceArray => {
             // email 记录是否已经存在
             if (dataCategoryExistanceArray.length > 0){
-                return res.send(new ResponseError('', '类别名已存在'))
+                return res.send(new ResponseError('', `${DATA_NAME}已存在`))
             } else {
                 utility
                     .verifyAuthorization(req)
@@ -37,7 +40,7 @@ router.post('/add', (req, res, next) => {
                             // query.name_en
                             let sqlArray = []
                             sqlArray.push(`
-                                insert into diary_category(name, name_en, color, sort_id, date_init) 
+                                insert into ${TABLE_NAME}(name, name_en, color, sort_id, date_init) 
                                 values('${req.body.name}', '${req.body.name_en}', '${req.body.color}', '${req.body.sort_id}', '${timeNow}')`
                             )
                             utility
@@ -47,11 +50,11 @@ router.post('/add', (req, res, next) => {
                                         utility.updateUserLastLoginTime(userInfo.uid)
                                         res.send(new ResponseSuccess({id: data.insertId}, '添加成功')) // 添加成功之后，返回添加后的日记 id
                                     } else {
-                                        res.send(new ResponseError('', '日记类别查询错误'))
+                                        res.send(new ResponseError('', `${DATA_NAME}查询错误`))
                                     }
                                 })
                                 .catch(err => {
-                                    res.send(new ResponseError(err, '类别添加失败'))
+                                    res.send(new ResponseError(err, `${DATA_NAME}添加失败`))
                                 })
                         } else {
                             res.send(new ResponseError('', '无权操作'))
@@ -75,7 +78,7 @@ router.put('/modify', (req, res, next) => {
                 // query.name_en
                 let sqlArray = []
                 sqlArray.push(`
-                    update diary_category set 
+                    update ${TABLE_NAME} set 
                     name = '${req.body.name}',
                     count = '${req.body.count}',
                     color = '${req.body.color}',
@@ -89,11 +92,11 @@ router.put('/modify', (req, res, next) => {
                             utility.updateUserLastLoginTime(userInfo.uid)
                             res.send(new ResponseSuccess({id: data.insertId}, '修改成功')) // 添加成功之后，返回添加后的日记类别 id
                         } else {
-                            res.send(new ResponseError('', '日记类别操作错误'))
+                            res.send(new ResponseError('', `${DATA_NAME}操作错误`))
                         }
                     })
                     .catch(err => {
-                        res.send(new ResponseError(err, '类别修改失败'))
+                        res.send(new ResponseError(err, `${DATA_NAME}修改失败`))
                     })
             } else {
                 res.send(new ResponseError('', '无权操作'))
@@ -112,7 +115,7 @@ router.delete('/delete', (req, res, next) => {
                 // query.name_en
                 let sqlArray = []
                 sqlArray.push(`
-                    delete from diary_category 
+                    delete from ${TABLE_NAME} 
                                where name_en = '${req.body.name_en}'
                     `)
                 utility
@@ -141,7 +144,7 @@ router.delete('/delete', (req, res, next) => {
 // 检查类别是否存在
 function checkCategoryExist(categoryName){
     let sqlArray = []
-    sqlArray.push(`select * from diary_category where name_en='${categoryName}'`)
+    sqlArray.push(`select * from ${TABLE_NAME} where name_en='${categoryName}'`)
     return utility.getDataFromDB( 'diary', sqlArray)
 }
 
