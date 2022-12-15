1de384e1e56909e763018ea2309fd4c070bcd9b1
邴新科 <kylebing@163.com>
2022-12-15 11:37:02 +0800

    update package.json packages

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
-const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 
@@ -14,7 +13,6 @@ app.set('view engine', 'pug')
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 
