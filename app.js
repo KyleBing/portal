f65a770783a398e7a556892f1f6847772018d0d3
KyleBing(MBP14) <kylebing@163.com>
2022-12-15 22:08:41 +0800

    set max_body_size = 50mb

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.use(logger('dev'))
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
