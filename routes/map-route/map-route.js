eb0e836c663f7e21bfd5298f37c32fa95819c612
邴新科-工作 <kylebing@163.com>
2023-03-08 18:12:43 +0800

    路线权限判断再操作

--- routes/map-route/map-route.js
+++ routes/map-route/map-route.js
@@ -35,38 +35,35 @@ router.post('/list', (req, res, next) => {
 
             let filterArray = []
 
+            // PUBLIC
+            filterArray.push(`is_public = 1 or ${CURRENT_DATABASE}.uid = ${userInfo.uid}`)
+
             // keywords
-            if (req.body.keyword){
+            if (req.body.keyword) {
                 let keywords = req.body.keyword.split(' ').map(item => utility.unicodeEncode(item))
-                if (keywords.length > 0){
-                    let keywordStrArray = keywords.map(keyword => `( ${CURRENT_DATABASE}.name like '%${keyword}%' ESCAPE '/'  or  ${CURRENT_DATABASE}.note like '%${keyword}%' ESCAPE '/' ` )
-                    filterArray.push( keywordStrArray.join(' and ')) // 在每个 categoryString 中间添加 'or'
+                if (keywords.length > 0) {
+                    let keywordStrArray = keywords.map(keyword => `( ${CURRENT_DATABASE}.name like '%${keyword}%' ESCAPE '/'  or  ${CURRENT_DATABASE}.note like '%${keyword}%' ESCAPE '/') `)
+                    filterArray.push(keywordStrArray.join(' and ')) // 在每个 categoryString 中间添加 'or'
                 }
             }
             // date range
-            if (req.body.dateRange && req.body.dateRange.length === 2){
-                if (filterArray.length > 0){
+            if (req.body.dateRange && req.body.dateRange.length === 2) {
+                if (filterArray.length > 0) {
                     filterArray.push(`and`)
                 }
                 filterArray.push(`date_init between '${req.body.dateRange[0]}' AND '${req.body.dateRange[1]}'`)
             }
-            // category
-            if (req.body.category_id){
-                if (filterArray.length > 0){
-                    filterArray.push(`and`)
-                }
-                filterArray.push(`category_id = ${req.body.category_id}`)
-            }
 
-            if (filterArray.length > 0){
+            if (filterArray.length > 0) {
                 filterArray.unshift('where')
             }
 
             let promisesAll = []
             let pointStart = (Number(req.body.pageNo) - 1) * Number(req.body.pageSize)
+            let sql = `${sqlBase} ${filterArray.join(' ')}  limit ${pointStart} , ${req.body.pageSize}`
             promisesAll.push(utility.getDataFromDB(
                 'diary',
-                [`${sqlBase} ${filterArray.join(' ')}  limit ${pointStart} , ${req.body.pageSize}`])
+                [sql])
             )
             promisesAll.push(utility.getDataFromDB(
                 'diary',
@@ -103,45 +100,31 @@ router.post('/list', (req, res, next) => {
 })
 
 router.get('/detail', (req, res, next) => {
-    let sqlArray = []
-    sqlArray.push(`select * from ${CURRENT_DATABASE} where id = '${req.query.id}'`)
-    utility
-        .getDataFromDB( 'diary', sqlArray, true)
-        .then(dataRoute => {
-            // decode unicode
-            dataRoute.name = utility.unicodeDecode(dataRoute.name)
-            dataRoute.note = utility.unicodeDecode(dataRoute.note)
-            // 2. 判断是否为共享 路线
-            if (dataRoute.is_public === 1){
-                // 2.1 如果是，直接返回结果，不需要判断任何东西
-                res.send(new ResponseSuccess(dataRoute))
+    Promise.all([
+        utility.verifyAuthorization(req),
+        utility.getDataFromDB('diary', [`select * from ${CURRENT_DATABASE} where id = ${req.query.id}`], true)
+    ])
+        .then(response => {
+            let userInfo = response[0]
+            let lineInfoData = response[1]
+            if (lineInfoData.uid === userInfo.uid
+                || userInfo.group_id === 1
+                || lineInfoData.is_public === 1
+            ){
+                res.send(new ResponseSuccess(lineInfoData))
             } else {
-                // 2.2 如果不是，需要判断：当前 email 和 token 是否吻合
-                utility
-                    .verifyAuthorization(req)
-                    .then(userInfo => {
-                        // 3. 判断 路线 是否属于当前请求用户
-                        if (Number(userInfo.uid) === dataRoute.uid){
-                            // 记录最后访问时间
-                            utility.updateUserLastLoginTime(userInfo.uid)
-                            dataRoute.name = utility.unicodeDecode(dataRoute.name)
-                            dataRoute.note = utility.unicodeDecode(dataRoute.note)
-                            res.send(new ResponseSuccess(dataRoute))
-                        } else {
-                            res.send(new ResponseError('','当前用户无权查看该 路线 ：请求用户 ID 与 路线 归属不匹配'))
-                        }
-                    })
-                    .catch(unverified => {
-                        res.send(new ResponseError('','当前用户无权查看该 路线 ：用户信息错误'))
-                    })
+                res.send(new ResponseError('', '该路线信息不属于您，无权操作'))
             }
         })
         .catch(err => {
-            res.send(new ResponseError(err, err.message))
+            res.send(new ResponseError(err, '服务器错误'))
         })
 })
 
 router.post('/add', (req, res, next) => {
+
+
+
     // 1. 验证用户信息是否正确
     utility
         .verifyAuthorization(req)
@@ -177,7 +160,7 @@ router.post('/add', (req, res, next) => {
                                 )
                         `)
                         utility
-                            .getDataFromDB( 'diary', sqlArray)
+                            .getDataFromDB('diary', sqlArray)
                             .then(data => {
                                 utility.updateUserLastLoginTime(userInfo.uid)
                                 res.send(new ResponseSuccess( // 添加成功之后，返回添加后的 路线  id
@@ -193,7 +176,7 @@ router.post('/add', (req, res, next) => {
                 })
                 .catch(err => {
                     res.send(new ResponseError(err, '查询路径记录出错'))
-            })
+                })
 
         })
         .catch(err => {
@@ -202,27 +185,30 @@ router.post('/add', (req, res, next) => {
 })
 
 // 检查用户名或邮箱是否存在
-function checkRouteExist(routeName){
+function checkRouteExist(routeName) {
     let sqlArray = []
     sqlArray.push(`select * from map_route where name='${routeName}'`)
-    return utility.getDataFromDB( 'diary', sqlArray)
+    return utility.getDataFromDB('diary', sqlArray)
 }
 
 
 router.put('/modify', (req, res, next) => {
 
-    // 1. 验证用户信息是否正确
-    utility
-        .verifyAuthorization(req)
-        .then(userInfo => {
-            let parsedName = utility.unicodeEncode(req.body.name) // !
-            let parsedNote = utility.unicodeEncode(req.body.note) || ''
-            let timeNow = utility.dateFormatter(new Date())
-            let sqlArray = []
-            sqlArray.push(`
+    Promise.all([
+        utility.verifyAuthorization(req),
+        utility.getDataFromDB('diary', [`select * from ${CURRENT_DATABASE} where id = ${req.body.id}`], true)
+    ])
+        .then(response => {
+            let userInfo = response[0]
+            let lineInfoData = response[1]
+            if (lineInfoData.uid === userInfo.uid || userInfo.group_id === 1){
+                let parsedName = utility.unicodeEncode(req.body.name) // !
+                let parsedNote = utility.unicodeEncode(req.body.note) || ''
+                let timeNow = utility.dateFormatter(new Date())
+                let sqlArray = []
+                sqlArray.push(`
                         update ${CURRENT_DATABASE}
-                            set
-                             name = '${parsedName}',
+                            set name = '${parsedName}',
                                 area = '${req.body.area || ""}',
                                 road_type = '${req.body.road_type || ""}',
                                 seasons = '${req.body.seasons || ""}',
@@ -233,85 +219,61 @@ router.put('/modify', (req, res, next) => {
                                 thumb_up = '${req.body.thumb_up || 0}'
                             WHERE id='${req.body.id}'
                     `)
-            utility
-                .getDataFromDB( 'diary', sqlArray, true)
-                .then(data => {
-                    utility.updateUserLastLoginTime(req.body.email)
-                    res.send(new ResponseSuccess(data, '修改成功'))
-                })
-                .catch(err => {
-                    res.send(new ResponseError(err, '修改失败'))
-                })
+                utility
+                    .getDataFromDB('diary', sqlArray, true)
+                    .then(data => {
+                        utility.updateUserLastLoginTime(req.body.email)
+                        res.send(new ResponseSuccess(data, '修改成功'))
+                    })
+                    .catch(err => {
+                        res.send(new ResponseError(err, '修改失败'))
+                    })
+
+            } else {
+                res.send(new ResponseError('', '该路线信息不属于您，无权操作'))
+            }
         })
         .catch(err => {
-            res.send(new ResponseError(err, '无权操作'))
+            res.send(new ResponseError(err, '服务器错误'))
         })
 })
 
 router.delete('/delete', (req, res, next) => {
-    // 1. 验证用户信息是否正确
-    utility
-        .verifyAuthorization(req)
-        .then(userInfo => {
-
-            let sqlArray = []
-            sqlArray.push(`
+    Promise.all([
+        utility.verifyAuthorization(req),
+        utility.getDataFromDB('diary', [`select * from ${CURRENT_DATABASE} where id = ${req.body.id}`], true)
+    ])
+        .then(response => {
+            let userInfo = response[0]
+            let lineInfoData = response[1]
+            if (lineInfoData.uid === userInfo.uid || userInfo.group_id === 1){
+                let sqlArray = []
+                sqlArray.push(`
                         DELETE from ${CURRENT_DATABASE}
                         WHERE id='${req.body.id}'
                     `)
-            if (userInfo.group_id !== 1){
-                sqlArray.push(` and uid='${userInfo.uid}'`) // 当为1管理员时，可以随意操作任意对象
-            }
-            utility
-                .getDataFromDB( 'diary', sqlArray)
-                .then(data => {
-                    if (data.affectedRows > 0) {
-                        utility.updateUserLastLoginTime(userInfo.uid)
-                        res.send(new ResponseSuccess('', '删除成功'))
-                    } else {
-                        res.send(new ResponseError('', '删除失败'))
-                    }
-                })
-                .catch(err => {
-                    res.send(new ResponseError(err,))
-                })
-
-        })
-        .catch(err => {
-            res.send(new ResponseError(err, '无权操作'))
-        })
-})
-
-router.post('/clear-visit-count', (req, res, next) => {
-    // 1. 验证用户信息是否正确
-    utility
-        .verifyAuthorization(req)
-        .then(userInfo => {
-            // 2. 是否为管理员
-            if(userInfo.group_id === 1) {
-                let sqlArray = []
-                let timeNow = utility.dateFormatter(new Date())
-                sqlArray.push(` update qrs set visit_count = 0 where hash = '${req.body.id}' `)
                 utility
-                    .getDataFromDB( 'diary', sqlArray)
+                    .getDataFromDB('diary', sqlArray)
                     .then(data => {
-                        utility.updateUserLastLoginTime(userInfo.uid)
-                        res.send(new ResponseSuccess('', '计数已清零')) // 添加成功之后，返回添加后的 路线  id
+                        if (data.affectedRows > 0) {
+                            utility.updateUserLastLoginTime(userInfo.uid)
+                            res.send(new ResponseSuccess('', '删除成功'))
+                        } else {
+                            res.send(new ResponseError('', '删除失败'))
+                        }
                     })
                     .catch(err => {
-                        console.log(err)
-                        res.send(new ResponseError(err, '计数清零失败'))
+                        res.send(new ResponseError(err,))
                     })
-            } else {
-                res.send(new ResponseError('', '无权操作'))
-            }
 
+            } else {
+                res.send(new ResponseError('', '该路线不属于您，无权操作'))
+            }
         })
         .catch(err => {
-            res.send(new ResponseError(err, '无权操作'))
+            res.send(new ResponseError(err, '服务器错误'))
         })
 })
-
 
 
 module.exports = router
