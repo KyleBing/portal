755e861640e73ff8afcc893cb844b7d6086fbc79
KyleBing <kylebing@163.com>
2023-09-19 14:14:00 +0800

    获取最近的带有 keyword 的公开日记内容

--- routes/diary/diary.js
+++ routes/diary/diary.js
@@ -322,6 +322,28 @@ router.get('/latest-recommend', (req, res, next) => {
         })
 })
 
+router.get('/get-latest-public-diary-with-keyword', (req, res, next) => {
+    let sqlArray = []
+    sqlArray.push(`select * from diaries where title like '%${req.params.keyword}%' and is_public = 1 and uid = 3 order by id desc`)
+    // 1. 先查询出日记结果
+    utility
+        .getDataFromDB('diary', sqlArray, false)
+        .then(diaryList => {
+            if (diaryList.length > 0) {
+                let dataDiary = diaryList[0]
+                // decode unicode
+                dataDiary.title = utility.unicodeDecode(dataDiary.title)
+                dataDiary.content = utility.unicodeDecode(dataDiary.content)
+                res.send(new ResponseSuccess(dataDiary))
+            } else {
+                res.send(new ResponseSuccess(''))
+            }
+        })
+        .catch(err => {
+            res.send(new ResponseError(err, '查询错误'))
+        })
+})
+
 router.post('/clear', (req, res, next) => {
     utility
         .verifyAuthorization(req)
