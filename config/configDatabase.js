d5659f364a796aa6f9ef7173f4dabc79faf16f4e
KyleBing(MBP14) <kylebing@163.com>
2024-01-06 23:48:50 +0800

    replace config file

---
+++ config/configDatabase.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host:       'localhost',
+    user:       '----',
+    password:   '----',
+    port:       '3306',
+    multipleStatements: true, // 允许同时请求多条 sql 语句
+    timezone: ''
+}
---
+++ config/configProject.js
@@ -0,0 +1,18 @@
+module.exports = {
+    invitation: '----', // 万能注册邀请码，用这个不需要使用系统生成的邀请码
+    adminCount: 'xxxx@163.com', // 超级管理员帐户，该用户可以在统计页面中查看所有用户统计数据
+
+    // 七牛云密钥
+    qiniuAccessKey: '',
+    qiniuSecretKey: '',
+
+    // 微信小程序开发者信息
+    wxMiniAppId: '',
+    wxMiniSecret: '',
+
+    // 微信公众号
+    wxToken: '',
+    wxPublicAppId: '',
+    wxPublicSecret: '',
+
+}
